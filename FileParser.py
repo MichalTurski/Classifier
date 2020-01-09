e480a5091995a9cfa4d5441f89e293a3d4d80583
MichalTurski <michal.antoni.turski@gmail.com>
2020-01-09 22:08:00 +0100

    Classifier training.

--- FileParser.py
+++ FileParser.py
@@ -1,6 +1,10 @@
 
 
-def sample_generator(file):
+def train_sample_generator(file):
     for line in file:
         if line[0] != '>':
             yield line.upper()
+
+
+def fasta_sample_generator(file):
+    pass
--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
 import click
 import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 import FrequencyCounter
 import FileParser
 
-
 SAMPLE_TYPE_KEY = 'sample_type'
 
 
-def file_to_freqs(file, sample_type):
+def train_file_to_freqs(file, sample_type):
     counts_list = []
-    for line in FileParser.sample_generator(file):
+    for line in FileParser.train_sample_generator(file):
         counts = FrequencyCounter.count_freq(line)
         counts[SAMPLE_TYPE_KEY] = sample_type
         counts_list.append(counts)
@@ -18,19 +20,33 @@ def file_to_freqs(file, sample_type):
     return df
 
 
-def create_freqs_df(neagtive_file, positive_file):
-    df_neg = file_to_freqs(neagtive_file, False)
-    df_pos = file_to_freqs(positive_file, True)
+def create_train_df(neagtive_file, positive_file):
+    df_neg = train_file_to_freqs(neagtive_file, False)
+    df_pos = train_file_to_freqs(positive_file, True)
     return pd.concat([df_neg, df_pos], ignore_index=True)
+
+
+def create_target_df(target_file):
+    pass
 
 
 @click.command()
 @click.option('--negative_samples_file', '-n', type=click.File('r'), required=True)
 @click.option('--positive_samples_file', '-p', type=click.File('r'), required=True)
 def main(negative_samples_file, positive_samples_file):
-    df = create_freqs_df(negative_samples_file, positive_samples_file)
-    print(df)
+    train_df = create_train_df(negative_samples_file, positive_samples_file)
+    train_df, test_df = train_test_split(train_df, test_size=0.25, random_state=123)
+    model = XGBClassifier()
+    model.fit(train_df.loc[:, train_df.columns != SAMPLE_TYPE_KEY], train_df.loc[:, SAMPLE_TYPE_KEY])
+    print(model)
 
+    y_pred = model.predict(test_df.iloc[:, train_df.columns != SAMPLE_TYPE_KEY])
+    predictions = [round(value) for value in y_pred]
+    print(test_df.loc[:, SAMPLE_TYPE_KEY])
+    accuracy = accuracy_score(test_df.loc[:, SAMPLE_TYPE_KEY].astype(int), predictions)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+    # print(train_df)
 
 if __name__ == "__main__":
     main()
